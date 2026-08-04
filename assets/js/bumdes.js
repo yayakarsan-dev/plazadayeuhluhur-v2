7b5da93d9e2e375267f3fbae4d58b8ae4840ec24
Yaya Karsan <yaya.karsan@gmail.com>
2026-08-04 18:46:02 +0700

    Update bumdes

--- assets/js/bumdes.js
+++ assets/js/bumdes.js
@@ -1,28 +1,104 @@
-fetch('data/bumdes.json')
-.then(res => res.json())
-.then(data=>{
+fetch('../data/bumdes.json')
 
-let html="";
+.then(response => response.json())
 
-data.forEach(item=>{
+.then(data => {
 
-html+=`
+let html = "";
 
-<div class="col-lg-4 mb-4">
+data.forEach(item => {
 
-<div class="card h-100 bumdes-card">
+html += `
 
-<img src="${item.gambar}" class="card-img-top">
+<div class="col-lg-4 col-md-6 mb-4">
+
+<div class="card shadow h-100 desa-card">
+
+<img src="../${item.gambar}" class="card-img-top">
 
 <div class="card-body">
 
-<h5>${item.nama}</h5>
+<span class="badge bg-success">
 
-<p><i class="fa-solid fa-location-dot text-success"></i> ${item.desa}</p>
+${item.status}
 
-<p>${item.usaha}</p>
+</span>
 
-<a href="#" class="btn btn-success w-100">
+<h4 class="mt-3">
+
+${item.nama}
+
+</h4>
+
+<p class="text-muted">
+
+📍 ${item.desa}
+
+</p>
+
+<div class="row">
+
+<div class="col-6">
+
+<div class="stat-box">
+
+🏢
+
+<b>${item.unit}</b>
+
+Unit Usaha
+
+</div>
+
+</div>
+
+<div class="col-6">
+
+<div class="stat-box">
+
+🛍
+
+<b>${item.produk}</b>
+
+Produk
+
+</div>
+
+</div>
+
+<div class="col-6">
+
+<div class="stat-box">
+
+👤
+
+<b>${item.direktur}</b>
+
+Direktur
+
+</div>
+
+</div>
+
+<div class="col-6">
+
+<div class="stat-box">
+
+✅
+
+<b>${item.status}</b>
+
+Status
+
+</div>
+
+</div>
+
+</div>
+
+<a href="${item.link}"
+
+class="btn btn-success w-100 mt-4">
 
 Lihat Profil
 
@@ -38,6 +114,8 @@ Lihat Profil
 
 });
 
-document.getElementById("bumdesContainer").innerHTML=html;
+document.getElementById("bumdesContainer").innerHTML = html;
 
-});
+})
+
+.catch(error => console.log(error));
